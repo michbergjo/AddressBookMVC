d37f110a113192bd753a6f4530180ed4d351bdd0
Michelle Joseph <mbergy27@gmail.com>
2021-09-01 15:45:24 -0500

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AddressBookMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AddressBookMVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+            base(options)
+        { 
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
